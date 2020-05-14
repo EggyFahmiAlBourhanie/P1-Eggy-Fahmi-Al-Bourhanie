7db7aa3775a9f32a802f094cb43c5a669a7e89ca
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 11:29:17 +0800

    Create jobsheet 2 C++.cpp
    Menampilkan nilai suatu variable

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float angka_1 = 1.5;
+	double angka_2 = 1.77777;
+	
+	cout <<"Contoh angka float= " <<angka_1; 
+	cout <<"\nContoh angka double= "<< angka_2;
+}
