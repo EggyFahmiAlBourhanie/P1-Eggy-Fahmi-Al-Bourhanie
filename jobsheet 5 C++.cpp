12e3e733ee10e973e4c27ac26851c7887b7ecaa1
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 11:31:23 +0800

    Create jobsheet 5 C++.cpp
    Input pada string

---
+++ jobsheet 5 C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	string nim;
+	cout <<"Masukan NIM kamu: ";
+	cin >> nim;
+	cout <<"Nomor Induk Mahasiswa anda adalah " << nim;
+	return 0;
+}
