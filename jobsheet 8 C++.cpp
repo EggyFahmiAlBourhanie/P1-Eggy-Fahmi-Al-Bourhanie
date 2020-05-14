bd281192ed4dc89f7f89f48396fbd2821df481cb
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 11:33:31 +0800

    Create jobsheet 8 C++.cpp
    Operator increment dan decrement dengan nilai statis

---
+++ jobsheet 8 C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float a, b;
+	double c;
+	c = a / b;
+	cout << "Masukan Nilai a: ";
+	cin >> a;
+	cout << "Masukan Nilai b: ";
+	cin >> b;
+	cout << "melakukan pembagian a/b =" << c;
+	
+	
+	cout<<"\nNilai c awal : "<< c;
+    cout<<"\nNilai --c    : "<<--c;
+    cout<<"\nNilai c     : "<< c;
+	return 0;
+}
