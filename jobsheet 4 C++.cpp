645e35cbe4a740d1da01ba2ad49d893533c7a8d2
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 11:30:46 +0800

    Create jobsheet 4 C++.cpp
    Input nilai pada suatu variabel

---
+++ jobsheet 4 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float bil_1;
+	double bil_2;
+	cout << "Masukan suatu bilangan float: ";
+	cin >> bil_1;
+	cout << "Bilangan float yang anda masukan = " << bil_1;
+	cout << "\nMasukan suatu bilangan double: ";
+	cin >> bil_2;
+	cout << "Bilangan double yang anda masukan = " << bil_2;
+}
