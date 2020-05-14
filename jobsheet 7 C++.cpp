4f77ea05ba7f83a49e321f83036de8b3150c2aa8
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-14 11:32:53 +0800

    Create jobsheet 7 C++.cpp
    Operator dengan nilai dinamis

---
+++ jobsheet 7 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;  
+int main()
+{
+	int a;
+	int b;
+	int c;
+	cout << "masukan nilai a: ";
+	cin >> a;
+	cout << "\nmasukan nilai b: ";
+	cin >> b;
+	c = a / b;
+	cout << "hasil a/b = " << c;
+	return 0;
+}
